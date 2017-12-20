0d7e07eb80684eb13d248fa9caa07355732c906e
Nov Matake <nov@matake.jp>
2017-12-20 16:41:36 +0900

    up

--- googleyolo.js
+++ googleyolo.js
@@ -1,6 +1,14 @@
 const GOOGLE_CLIENT_ID = '97329362365-hbe147jo6o1m56e316efsp89d6s22k46.apps.googleusercontent.com';
 
 let app = {};
+let elements {
+  welcome: document.querySelector('#welcome'),
+  id_token: {
+    header: document.querySelector('#idtoken .header'),
+    payload: document.querySelector('#idtoken .payload')
+  },
+  forget_me: document.querySelector('#forget-me')
+}
 
 app.signIn = () => {
   return googleyolo.retrieve({
@@ -19,14 +27,20 @@ app.signIn = () => {
 
 app.signedIn = (credential) => {
   console.info('signed-in with', credential);
-  document.querySelector('#welcome').innerText = "Welcome, " + credential.displayName + '!';
+  elements.welcome.innerText = "Welcome, " + credential.displayName + '!';
 
   let [header, payload, signature] = credential.idToken.split('.');
   let prettyPrint = (jwt_segment) => {
     return JSON.stringify(JSON.parse(atob(jwt_segment)), false, 2);
   };
-  document.querySelector('#idtoken .header').innerText = prettyPrint(header);
-  document.querySelector('#idtoken .payload').innerText = prettyPrint(payload);
+  elements.id_token.header.innerText = prettyPrint(header);
+  elements.id_token.payload.innerText = prettyPrint(payload);
+
+  elements.forget_me.onclick = () => {
+    app.signOut().then(() => {
+      location.reload();
+    })
+  }
 };
 
 app.signUp = () => {
